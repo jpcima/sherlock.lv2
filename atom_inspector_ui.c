677db962b41cff95aef64ce923bf0d5de15cca85
Hanspeter Portner <dev@open-music-kontrollers.ch>
2015-04-22 18:49:18 +0200

    fix minimum dimension

--- atom_inspector_ui.c
+++ atom_inspector_ui.c
@@ -563,8 +563,8 @@ instantiate(const LV2UI_Descriptor *descriptor, const char *plugin_uri,
 	eo_ui_t *eoui = &ui->eoui;
 	eoui->driver = driver;
 	eoui->content_get = _content_get;
-	eoui->w = 400,
-	eoui->h = 400;
+	eoui->w = 500,
+	eoui->h = 500;
 
 	ui->write_function = write_function;
 	ui->controller = controller;
