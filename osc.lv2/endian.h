33193a5b9b23c49e39e411bb2dab5670d67da5a6
Hanspeter Portner <dev@open-music-kontrollers.ch>
2017-08-30 10:00:30 +0200

    Squashed 'osc.lv2/' changes from 9f3e14b..3a6e2ea
    3a6e2ea Properly handle OpenBSD.
    
    git-subtree-dir: osc.lv2
    git-subtree-split: 3a6e2ea6793f308e90cac353b4d9a9a5345f5cb8

--- osc.lv2/endian.h
+++ osc.lv2/endian.h
@@ -45,7 +45,7 @@
 
 #	include <sys/endian.h>
 
-#elif defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
+#elif defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__) || defined(__OpenBSD__)
 
 #	include <sys/endian.h>
 
