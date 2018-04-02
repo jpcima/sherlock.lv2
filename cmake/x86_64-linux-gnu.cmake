de3879efd86b7f0c1ae0d0156e08bd03fd61b1e4
Hanspeter Portner <dev@open-music-kontrollers.ch>
2018-04-02 12:36:42 +0200

    initial migration to meson.

--- cmake/x86_64-linux-gnu.cmake
+++
@@ -1,8 +0,0 @@
-# the name of the target operating system
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR "x86_64")
-set(TOOLCHAIN "x86_64-linux-gnu")
-
-set(STATIC_SRATOM "/opt/${TOOLCHAIN}/lib/libsratom-0.a")
-set(STATIC_SERD "/opt/${TOOLCHAIN}/lib/libserd-0.a")
-set(STATIC_SORD "/opt/${TOOLCHAIN}/lib/libsord-0.a")
